28781e8d45bbb2f85a8378c0b44421a5f91aa8e7
Kevin Candra <candra@radsoftsystem.com>
2023-12-19 14:23:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/respon/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.respon;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
+import com.example.respon.databinding.ActivityMainBinding;
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Pesan Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btSnack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                @SuppressLint("ShowToast") Snackbar snackbar = Snackbar
+                        .make(v, "Snack Bar!", Snackbar.LENGTH_LONG)
+                        .setAction("OK", new View.OnClickListener(){
+                            @Override
+                            public void onClick(View view){
+                                Toast.makeText(getApplicationContext(), "Snack Bar menutup", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                snackbar.setActionTextColor(Color.BLUE);
+                snackbar.show();
+            }
+        });
+
+        binding.btDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder.setMessage("Ingin menutup activity ini?");
+                alertDialogBuilder.setPositiveButton("Ya",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                finish();
+                            }
+                        });
+
+                alertDialogBuilder.setNegativeButton("Tidak",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+
+        binding.btNotifikasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder mBuilder = new
+                        NotificationCompat.Builder(MainActivity.this, "channel1")
+                                .setSmallIcon(R.drawable.baseline_refresh_24)
+                                .setContentTitle("Android!")
+                                .setContentText("Anda sedang mempelajarinya");
+
+                NotificationManager mNotificationManager = (NotificationManager)
+                        getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    int importance = NotificationManager.IMPORTANCE_HIGH;
+                    NotificationChannel notificationChannel = new
+                            NotificationChannel("channel2", "contoh channel", importance);
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    mBuilder.setChannelId("channel2");
+                    assert mNotificationManager != null;
+                    mNotificationManager.createNotificationChannel(notificationChannel);
+                }
+
+                assert mNotificationManager != null;
+                mNotificationManager.notify((int) System.currentTimeMillis(),
+                        mBuilder.build());
+            }
+        });
+    }
+}
