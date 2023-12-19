7a1dfc937f07208c17b2c6dc245ba8f2abc1256c
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2023-12-20 06:35:34 +0700

    upgrade

--- app/src/main/java/com/example/respon/MainActivity.java
+++ app/src/main/java/com/example/respon/MainActivity.java
@@ -6,14 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 import com.example.respon.databinding.ActivityMainBinding;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.Color;
@@ -81,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
         binding.btNotifikasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NotificationCompat.Builder mBuilder = new
+                // menambahkan notifikasi jika belum mengaktifkan izin Notificatin
+                if (NotificationManagerCompat.from(MainActivity.this).areNotificationsEnabled()) {
+                    NotificationCompat.Builder mBuilder = new
                         NotificationCompat.Builder(MainActivity.this, "channel1")
                                 .setSmallIcon(R.drawable.baseline_refresh_24)
                                 .setContentTitle("Android!")
@@ -104,6 +111,33 @@ public class MainActivity extends AppCompatActivity {
                 assert mNotificationManager != null;
                 mNotificationManager.notify((int) System.currentTimeMillis(),
                         mBuilder.build());
+                } else {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertDialogBuilder.setMessage("Ingin Mengizinkan Notifikasi?");
+                    alertDialogBuilder.setPositiveButton("Ya",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                    // buka notification setting
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                    intent.setData(uri);
+                                    startActivity(intent);
+                                }
+                            });
+
+                    alertDialogBuilder.setNegativeButton("Tidak",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+                }
             }
         });
     }
